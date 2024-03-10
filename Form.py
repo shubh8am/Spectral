2f54c9a219f04265c62d5802181219f960718fbb
Shubham <70328845+shubh8am@users.noreply.github.com>
2024-03-11 00:28:15 +0530

    Create Form.py

---
+++ Form.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Simple Form Example")
+name = st.text_input("Enter your name")
+country = st.selectbox("Select your country", ["USA", "Canada", "UK", "Australia"])
+gender = st.radio("Select your gender", ["Male", "Female", "Other"])
+birthdate = st.date_input("Enter your birthdate")
+if st.button("Submit"):
+  st.success(f"Name: {name}, Country: {country}, Gender: {gender}, Birthdate: {birthdate}")
